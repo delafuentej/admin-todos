b993fb6a823a3500bc18814d7bd3c456445080a5
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-11 17:51:01 +0200

    simple pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,7 +3,17 @@ import { NextResponse, NextRequest } from 'next/server';
 
 
 export async function GET(request: Request) { 
+    const {searchParams} = new URL(request.url);
+    const take = Number(searchParams.get('take') ?? '10');
+    const skip = Number(searchParams.get('skip') ?? '0');
+    if(isNaN(take) || isNaN(skip)) {
+        return NextResponse.json({ message: 'The value must be a number'},{status:400})
+    }
 
-  const todos = await prisma.todo.findMany();
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+
+  });
   return NextResponse.json(todos);
 }
