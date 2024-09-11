6ae83dffae4f491f867a1004773ffa9e320328cd
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-11 18:46:20 +0200

    route: creating a todo

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -9,11 +9,20 @@ export async function GET(request: Request) {
     if(isNaN(take) || isNaN(skip)) {
         return NextResponse.json({ message: 'The value must be a number'},{status:400})
     }
-
+[]
   const todos = await prisma.todo.findMany({
     take: take,
     skip: skip,
 
   });
   return NextResponse.json(todos);
+}
+
+
+
+export async function POST(request: Request) { 
+    const body = await request.json();
+
+    const todo = await prisma.todo.create({data: body});
+  return NextResponse.json(todo);
 }
