599ea395a46b9c9bf2c9f4a2050f0c7afd160e64
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-11 20:05:19 +0200

    update method: to update entries

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -16,3 +16,22 @@ export async function GET(request: Request, {params}: Args) {
     console.log('id',params.id)
  return NextResponse.json(todo);
 }
+
+
+
+export async function PUT(request: Request, {params}: Args) { 
+    const { id }= params;
+    const todo = await prisma.todo.findFirst({where: {id}})
+    if(!todo) {
+     return NextResponse.json({message: `Todo with id: ${id} doesn't exists`},{status: 404})
+    }
+    const body = await request.json();
+
+    const updatedTodo = await prisma.todo.update({
+        where: {id},
+        data: {...body}
+    })
+     console.log('id',params.id)
+  return NextResponse.json(updatedTodo);
+    
+}
