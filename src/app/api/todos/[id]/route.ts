7d867a4ef3ab6f4a98ae8524506bc8d610e2d945
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-11 20:34:59 +0200

    validation with yup: put method

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,37 +1,56 @@
 import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
+import { Todo } from '@prisma/client';
+import { NextResponse, NextRequest } from 'next/server';
+import * as yup from 'yup';
 
 interface Args {
     params: {
         id: string;
     }
 }
+
+const getTodo = async(id: string): Promise<Todo | null> => {
+    const todo = await prisma.todo.findFirst({where: {id}});
+    return todo;
+}
 export async function GET(request: Request, {params}: Args) { 
    // console.log(request.url);
-   const { id }= params;
-   const todo = await prisma.todo.findFirst({where: {id}})
+ // const { id }= params;
+   const todo = await getTodo(params.id)
    if(!todo) {
-    return NextResponse.json({message: `Todo with id: ${id} doesn't exists`},{status: 404})
+    return NextResponse.json({message: `Todo with id: ${params.id} doesn't exists`},{status: 404})
    }
     console.log('id',params.id)
  return NextResponse.json(todo);
 }
 
 
+const putSchema = yup.object({
+    description: yup.string().optional(),
+    complete: yup.boolean().optional(),
+})
+
 
 export async function PUT(request: Request, {params}: Args) { 
-    const { id }= params;
-    const todo = await prisma.todo.findFirst({where: {id}})
+    //const { id }= params;
+    const todo = await getTodo(params.id)
     if(!todo) {
-     return NextResponse.json({message: `Todo with id: ${id} doesn't exists`},{status: 404})
+     return NextResponse.json({message: `Todo with id: ${params.id} doesn't exists`},{status: 404})
     }
-    const body = await request.json();
 
-    const updatedTodo = await prisma.todo.update({
-        where: {id},
-        data: {...body}
-    })
-     console.log('id',params.id)
-  return NextResponse.json(updatedTodo);
+    try{
+        const {description, complete} = await putSchema.validate(await request.json());
+
+        const updatedTodo = await prisma.todo.update({
+            where: {id: params.id},
+            data: {description, complete}
+        })
+        // console.log('id',params.id)
+      return NextResponse.json(updatedTodo);
+
+    }catch(error){
+        return NextResponse.json(error, {status: 400})
+    }
+   
     
 }
