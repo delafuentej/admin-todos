404383e007345802607e85c33e9375b744c771c6
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-14 11:46:41 +0200

    list entries from server side

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,29 @@
+import prisma from "@/lib/prisma";
+import { Metadata } from "next";
+//'use client';
+//import { useEffect } from "react";
 
-export default function RestTodos() {
+export const metadata: Metadata = {
+  title : "Todo's List",
+  description: "This is Todo's List"
+}
+
+
+export default async function RestTodos() {
+  const todos = await prisma.todo.findMany({
+    orderBy: {description: 'asc'}
+  })
+  
+
+  // useEffect(()=>{
+  //     fetch('/api/todos')
+  //     .then( res => res.json())
+  //     .then(console.log)
+  // }, []);
+  
   return (
     <div>
-      <h1>RestTodos Page</h1>
+     {JSON.stringify(todos)}
     </div>
   );
 }
