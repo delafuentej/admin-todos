f4fa1472d5b6e14ca75c892d02ee303d047c8a19
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-16 14:19:35 +0200

    shopping-cart from server-side

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci';
+import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci';
+import { cookies } from 'next/headers';
+
+
 
 export const TopMenu = () => {
+
+  const cookiesStore = cookies();
+  const cart = JSON.parse(cookiesStore.get('cart')?.value ?? '{}');
+
+  const getTotalCount = () => {
+    let items = 0;
+
+    Object.values(cart).forEach( (val) => {
+      items += val as number;
+    })
+
+    return items;
+  }
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
 
@@ -28,7 +44,7 @@ export const TopMenu = () => {
           <CiChat1 size={25} />
         </button>
         <button className="flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200 p-2">
-          <span className='text-sm mr-2 text-blue-700 font-bold'>2</span>
+          <span className='text-sm mr-2 text-blue-700 font-bold'>{getTotalCount()}</span>
           <CiShoppingBasket size={25}/>
         </button>
       </div>
